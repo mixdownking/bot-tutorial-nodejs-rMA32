b6706a3f04b13b852527bf6db9750ee6f8dd2f5b
mixdownking <mixdownking@gmail.com>
2015-12-31 03:54:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ function respond() {
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/GGML2016/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://daddyleagues.com/MASFL/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
@@ -33,7 +33,7 @@ function respond() {
   }
   else if(request.text && botRegexRules.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.daddyleagues.com/GGML2016/rules");
+    postMessage("http://www.daddyleagues.com/MASFL/rules");
     this.res.end();
   } 
   else if(request.text && botRegexGTA.test(request.text)) {
@@ -43,14 +43,14 @@ function respond() {
   } 
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/GGML2016/team/"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://daddyleagues.com/MASFL/team/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/GGML2016/players?name="+rep+"&position=all&team=all");
+    postMessage("http://daddyleagues.com/MASFL/players?name="+rep+"&position=all&team=all");
     this.res.end();
   }  
 
